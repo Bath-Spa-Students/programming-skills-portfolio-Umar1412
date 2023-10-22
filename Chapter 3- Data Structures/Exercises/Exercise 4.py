d7dd95bc5b17b991b648b3f3ca6b0074633c775b
Umar1412 <umarasif1412@gmail.com>
2023-10-22 16:39:31 +0400

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -0,0 +1,13 @@
+# If you could invite anyone, living or deceased, to dinner, who would you invite?
+ 
+# Make a list that includes at least three people you’d like to invite to dinner.
+
+# Then use your list to print a message to each person, inviting them to dinner.
+
+guest_list = ['Faisal', 'Wathila', 'Saeed']
+
+print(f"{guest_list[0].title()}, you are invited to attend my dinner.")
+
+print(f"{guest_list[1].title()}, you are invited to attend my dinner.")
+
+print(f"{guest_list[2].title()}, you are invited to attend my dinner.")
