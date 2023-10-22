1ef7d2e6b381fbce0ed78c9c34cc71bb0d0d03e1
Umar1412 <umarasif1412@gmail.com>
2023-10-22 16:01:59 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,14 @@
+#Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. 
+
+#The text of each message should be the same, but each message should be personalized with the person’s name.
+
+friends_names = ['Faisal', 'Wathila', 'Saeed']
+
+text = f"How are you, {friends_names[0].title()}?"
+print(text)
+
+text = f"How are you, {friends_names[1].title()}?"
+print(text)
+
+text = f"How are you, {friends_names[2].title()}?"
+print(text )
