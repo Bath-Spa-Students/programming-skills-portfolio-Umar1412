f548e5f2f30df7ed374a5aac35e1e415cf93e46c
Umar1412 <umarasif1412@gmail.com>
2023-10-22 15:33:09 +0400

    Excercise 4.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -1,5 +1,8 @@
 # Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print that message.
 
 fav_number = int("14")
+fav_number = 14
 
-print (int(" My favourite number is " + fav_number ))
+print (int(" My favourite number is " + fav_number ))output = f"My favourite number is {fav_number}."
+
+print(output)
