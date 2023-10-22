ce65c203d7b0ed4523dcbb2b421472c7e5394631
Umar1412 <umarasif1412@gmail.com>
2023-10-22 17:43:10 +0400

    Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,28 @@
+# You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. 
+
+# You’ll have to think of someone else to invite.
+
+# •Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
+
+# •Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+# •Print a second set of invitation messages, one for each person who is still in your list.
+
+guest_list = ['Faisal', 'Wathila', 'Saeed']
+
+print(f"{guest_list[0].title()}, you are invited to attend my dinner.")
+
+print(f"{guest_list[1].title()}, you are invited to attend my dinner.")
+
+print(f"{guest_list[2].title()}, you are invited to attend my dinner.")
+
+print(f"\nSorry, {guest_list[2].title()} can't attend the dinner. ")
+
+del(guest_list[2])
+guest_list.insert(2, 'Ali')
+
+print(f"\n{guest_list[0].title()}, you are invited to attend my dinner.")
+
+print(f"{guest_list[1].title()}, you are invited to attend my dinner.")
+
+print(f"{guest_list[2].title()}, you are invited to attend my dinner.")
