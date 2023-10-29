7d4e7d1e4247e3944baf0a10fc6b483bf7737e5e
Umar1412 <umarasif1412@gmail.com>
2023-10-29 20:00:30 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,31 @@
+# Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+
+# •Make a list of your three favorite fruits and call it favorite_fruits.
+
+# •Write five if statements. Each should check whether a certain kind of fruit is in your list.
+
+# If the fruit is in your list, the if block should print a statement,such as You really like bananas!
+
+
+# List of my favoruite fruits:
+
+favorite_fruits = ['Mangoes','Strawberries','Apples']
+
+# If statements: 
+
+if 'Strawberries' in favorite_fruits: 
+    print("You really like Strawberries!")
+
+if 'Pineapples' in favorite_fruits:
+    print("You really like Pineapples!")
+
+if 'Mangoes' in favorite_fruits:
+    print("You really like Mangoes!")
+
+if 'Peaches' in favorite_fruits:
+    print("You really like Peaches!")
+
+if 'Apples' in favorite_fruits:
+    print("You really like Apples!")
+
+
