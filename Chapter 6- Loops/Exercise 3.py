7e20bf8c6806bc11b2c3aa0cf19e69eacc2422f5
Umar1412 <umarasif1412@gmail.com>
2023-11-15 23:23:28 +0400

    Exercise 3.py

---
+++ Chapter 6- Loops/Exercise 3.py
@@ -0,0 +1,7 @@
+# Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.)
+
+
+# Never ending loop
+
+while True:
+    print("This is an endless loop")
