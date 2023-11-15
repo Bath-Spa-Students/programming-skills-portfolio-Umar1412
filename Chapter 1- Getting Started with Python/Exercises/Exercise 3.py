fc06bf6432845b50f0e8761445656341ef6dd204
Umar1412 <144319790+Umar1412@users.noreply.github.com>
2023-11-16 00:09:53 +0400

    Update Exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
@@ -2,3 +2,8 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time in The UAE =", current_time)
+
+from datetime import date
+
+today = date.today()
+print("The date today is:", today) 
